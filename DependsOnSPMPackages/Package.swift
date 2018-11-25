881fa019bcc89dc9728d548d45ad011f8f6a6477
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-11-24 23:56:59 -0200

    remove app

--- DependsOnSPMPackages/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "DependsOnSPMPackages",
-    dependencies: [
-        .Package(url: "https://github.com/marinofelipe/swift-package-manager-tryout/tree/basics/packages/firstPackage", from: "1.0.0")
-    ]
-)
